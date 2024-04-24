367c7d2e9b1b3189b05da51e0404f598edcbdb75
kametsun <kametsun7@gmail>
2024-04-24 22:16:00 +0900

    * HeaderとFooterにラインを追加

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,13 @@ import { ReactElement } from "react";
 function Footer(): ReactElement {
   return (
     <footer>
-      <Center h={"10"} bg={"whiteAlpha.50"} color={"blackAlpha.950"}>
+      <Center
+        h={"10"}
+        bg={"whiteAlpha.50"}
+        color={"blackAlpha.950"}
+        borderTop={"solid"}
+        borderColor={"blackAlpha.300"}
+      >
         <Heading size={"xs"}>© 2024 kametsun</Heading>
       </Center>
     </footer>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
-import { Box, Flex, Heading, Spacer } from "@yamada-ui/react";
+import { Box, Flex, Heading } from "@yamada-ui/react";
 import { ReactElement } from "react";
 
 function Header(): ReactElement {
   return (
     <header>
-      <Box p={"3"} bg={"whiteAlpha.50"} color={"blackAlpha.950"}>
+      <Box
+        p={"3"}
+        bg={"whiteAlpha.50"}
+        color={"blackAlpha.950"}
+        borderBottom={"solid"}
+        borderColor={"blackAlpha.300"}
+      >
         <Flex>
           <Heading size={"md"}>Welcome to Tsubasa's page!</Heading>
-          <Spacer />
-          <Box>テスト</Box>
         </Flex>
       </Box>
     </header>
