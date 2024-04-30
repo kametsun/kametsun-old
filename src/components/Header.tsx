4b0fd8a898b24e3778d0a4f8f80147b00d6fe8dd
kametsun <kametsun7@gmail>
2024-04-30 09:02:58 +0900

    * headerを固定化

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,21 @@ import Navigation from "./Navigation";
 
 function Header(): ReactElement {
   return (
-    <header>
+    <>
       <Box
+        as="header"
         p={"3"}
         bg={"whiteAlpha.50"}
+        shadow={"lg"}
+        boxShadow={"3xl"}
         color={"blackAlpha.950"}
         borderBottom={"solid"}
         borderColor={"blackAlpha.300"}
+        position={"fixed"}
+        top={0}
+        left={0}
+        right={0}
+        zIndex={999}
       >
         <Flex>
           <Link href="/" color={"blackAlpha.950"} _hover={"none"}>
@@ -20,7 +28,8 @@ function Header(): ReactElement {
           <Navigation />
         </Flex>
       </Box>
-    </header>
+      <Box pt={"calc(3rem + 2px)"} />
+    </>
   );
 }
 
