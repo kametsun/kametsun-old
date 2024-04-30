26760927b9136d3eccf9507202e4f5a37ff1a170
kametsun <kametsun7@gmail>
2024-04-30 09:03:54 +0900

    * 余分なタグを削除

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,17 +3,16 @@ import { ReactElement } from "react";
 
 function Footer(): ReactElement {
   return (
-    <footer>
-      <Center
-        h={"10"}
-        bg={"whiteAlpha.50"}
-        color={"blackAlpha.950"}
-        borderTop={"solid"}
-        borderColor={"blackAlpha.300"}
-      >
-        <Heading size={"xs"}>© 2024 kametsun</Heading>
-      </Center>
-    </footer>
+    <Center
+      as="footer"
+      h={"10"}
+      bg={"whiteAlpha.50"}
+      color={"blackAlpha.950"}
+      borderTop={"solid"}
+      borderColor={"blackAlpha.300"}
+    >
+      <Heading size={"xs"}>© 2024 kametsun</Heading>
+    </Center>
   );
 }
 
